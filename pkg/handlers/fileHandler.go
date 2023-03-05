f69fb5fb1061cc445ea394e3930a4650a745e54e
Kai Po Chen <neko12377+github@gmail.com>
2023-03-05 19:30:35 +0800

    Add functionality to reapond with directory constents as json and file contents as []byte

--- pkg/handlers/fileHandler.go
+++ pkg/handlers/fileHandler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"io/ioutil"
@@ -28,6 +29,11 @@ var (
 	FileName     = OrderConditon{"fileName"}
 )
 
+type Directory struct {
+	IsDirectory bool     `jason:"isDirectory"`
+	Files       []string `jason:"files"`
+}
+
 func FileHandler(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
 	localSystemFilePath := queryParams.Get("localSystemFilePath")
@@ -37,17 +43,41 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 
 	files, err := getFile(localSystemFilePath)
 	if err != nil {
+		if strings.Contains(err.Error(), "not a directory") {
+			content, err := readFile(localSystemFilePath)
+			if err != nil {
+				return
+			}
+			w.Write(content)
+			return
+		}
+
+		http.ServeFile(w, r, "../../static/404.html")
 		return
 	}
+
 	if orderBy.value != "" || orderByDirection.value != "" {
 		files = sortFiles(files, orderBy, orderByDirection)
 	}
 	if filterByName != "" {
 		files = filterFilesByName(files, filterByName)
 	}
-	for _, file := range files {
-		fmt.Printf("%s (%d bytes)(modified %v)\n", file.Name(), file.Size(), file.ModTime())
+
+	directory := Directory{
+		IsDirectory: true,
+		Files:       []string{},
 	}
+	w.Header().Set("Content-Type", "application/json")
+	for _, file := range files {
+		directory.Files = append(directory.Files, formatFileInfo(file))
+	}
+
+	jsonData, err := json.Marshal(directory)
+	if err != nil {
+		println("Error:", err)
+		return
+	}
+	w.Write(jsonData)
 }
 
 func getFile(localSystemFilePath string) ([]fs.FileInfo, error) {
@@ -56,6 +86,44 @@ func getFile(localSystemFilePath string) ([]fs.FileInfo, error) {
 		return nil, err
 	}
 	return files, nil
+}
+
+func readFile(localSystemFilePath string) ([]byte, error) {
+	content, err := ioutil.ReadFile(localSystemFilePath)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
+}
+
+func formatFileInfo(file fs.FileInfo) string {
+	name := file.Name()
+	size := file.Size()
+	modTime := file.ModTime().Format("2006-01-02 15:04:05")
+
+	return fmt.Sprintf("%s (%s) (%s)\n", name, formatSize(size), modTime)
+}
+
+func formatSize(size int64) string {
+	const (
+		kb = 1024
+		mb = kb * 1024
+		gb = mb * 1024
+		tb = gb * 1024
+	)
+
+	switch {
+	case size >= tb:
+		return fmt.Sprintf("%.2f TB", float64(size)/tb)
+	case size >= gb:
+		return fmt.Sprintf("%.2f GB", float64(size)/gb)
+	case size >= mb:
+		return fmt.Sprintf("%.2f MB", float64(size)/mb)
+	case size >= kb:
+		return fmt.Sprintf("%.2f KB", float64(size)/kb)
+	default:
+		return fmt.Sprintf("%d bytes", size)
+	}
 }
 
 func sortFiles(files []fs.FileInfo, orderConditon OrderConditon, orderDirection OrderDirection) []fs.FileInfo {
